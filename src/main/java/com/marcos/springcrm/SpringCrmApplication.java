74a659e884f0ddd1929686b2734e9c963f489304
Mlira02 <liram1995@gmail.com>
2021-06-14 10:52:09 -0500

    Initial commit and setup

---
+++ src/main/java/com/marcos/springcrm/SpringCrmApplication.java
@@ -0,0 +1,15 @@
+package com.marcos.springcrm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCrmApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(SpringCrmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/marcos/springcrm/SpringCrmApplicationTests.java
@@ -0,0 +1,15 @@
+package com.marcos.springcrm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCrmApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
